f5ab93ab26e909728488f2cfbde0546b9e609aef
cwallenwein <chris.wallenwein@gmail.com>
2023-01-18 09:24:51 +0100

    get selected text

--- worker.js
+++ worker.js
@@ -14,9 +14,23 @@ chrome.runtime.onInstalled.addListener(() => {
 })
 
 // listen to requests (from popup)
-chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
+chrome.runtime.onMessage.addListener(async (message, sender, sendResponse) => {
     if (message.type === "summarization_request"){
         let tabId = message.tabId
         console.log("Summarization request from tab", tabId)
+        let text = await getSelectedText(tabId)
+        console.log(text)
     }
 })
+
+async function getSelectedText(tabId){
+    let injectionResults = await chrome.scripting.executeScript({
+        target: {tabId: tabId},
+        func: () => window.getSelection().toString(),
+    })
+    if(injectionResults && injectionResults.length >= 1){
+        let selection = injectionResults[0].result
+        return selection
+    }
+    else return ""
+}
