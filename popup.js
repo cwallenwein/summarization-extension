57378bab7db4c461b86e9e42c633bd3562b9e49d
cwallenwein <chris.wallenwein@gmail.com>
2023-01-19 15:30:16 +0100

    format code

--- popup.js
+++ popup.js
@@ -8,54 +8,54 @@ document.addEventListener("DOMContentLoaded", async () => {
   console.log(apiKey)
 })
 
-function addSummarizeButtonEventListener(){
+function addSummarizeButtonEventListener() {
   document.getElementById("button-summarize").addEventListener("click", requestSummary);
 }
 
-async function requestSummary(){
+async function requestSummary() {
   let activeTab = await getActiveTab();
   let tabId = activeTab.id
   let url = activeTab.url
-  await chrome.runtime.sendMessage({type:"summarization_request", url: url, tabId: tabId})
+  await chrome.runtime.sendMessage({ type: "summarization_request", url: url, tabId: tabId })
 }
 
-async function getActiveTab(){
+async function getActiveTab() {
   let queryOptions = { active: true, lastFocusedWindow: true };
   // `tab` will either be a `tabs.Tab` instance or `undefined`.
   let [tab] = await chrome.tabs.query(queryOptions);
   return tab;
 }
 
-async function buildApiKeyForm(){
+async function buildApiKeyForm() {
   let apiKeyForm = document.getElementById("api-key-form")
   console.log(apiKeyForm)
   apiKeyForm.addEventListener("submit", async (event) => {
     console.log("submitting api key")
     event.preventDefault()
     let apiKey = document.getElementById("api-key-input").value
-    await chrome.storage.local.set({apiKey: apiKey})
+    await chrome.storage.local.set({ apiKey: apiKey })
   })
 }
 
 chrome.storage.onChanged.addListener(async (changes, namespace) => {
   console.log("History was updated!")
-  for (key in changes){
-    if(key === "history"){
+  for (key in changes) {
+    if (key === "history") {
       await buildSummaryHistory()
     }
   }
 })
 
-async function buildSummaryHistory(){
+async function buildSummaryHistory() {
   let history = await getHistory()
   console.log("new history", history)
 
   let headerRow = buildHeaderRow()
-  
+
   let tableBody = document.createElement("tbody")
   tableBody.appendChild(headerRow)
 
-  for (let i = history.length - 1; i >= 0; i--){
+  for (let i = history.length - 1; i >= 0; i--) {
     let row = buildRowFromSummary(history[i])
     tableBody.appendChild(row)
   }
@@ -66,21 +66,21 @@ async function buildSummaryHistory(){
   table.replaceChildren(tableBody)
 }
 
-async function getHistory(){
+async function getHistory() {
   let result = await chrome.storage.local.get("history")
   return result.history
 }
 
-async function getApiKey(){
+async function getApiKey() {
   let result = await chrome.storage.local.get("apiKey")
   return result.apiKey
 }
 
-function buildRowFromSummary(summary){
+function buildRowFromSummary(summary) {
   let urlCell = builtTableData(summary.url)
   let textCell = builtTableData(summary.text)
   let summaryCell = builtTableData(summary.summary)
-  
+
   let row = document.createElement("tr")
   row.appendChild(urlCell)
   row.appendChild(textCell)
@@ -88,11 +88,11 @@ function buildRowFromSummary(summary){
   return row
 }
 
-function buildHeaderRow(){
+function buildHeaderRow() {
   let url = buildTableHeader("URL")
   let text = buildTableHeader("Text")
   let summary = buildTableHeader("Summary")
-  
+
   let row = document.createElement("tr")
   row.appendChild(url)
   row.appendChild(text)
@@ -100,13 +100,13 @@ function buildHeaderRow(){
   return row
 }
 
-function buildTableHeader(content){
+function buildTableHeader(content) {
   let header = document.createElement("th")
   header.innerHTML = content
   return header
 }
 
-function builtTableData(content){
+function builtTableData(content) {
   let data = document.createElement("td")
   data.innerHTML = content
   return data
--- worker.js
+++ worker.js
@@ -8,7 +8,7 @@ chrome.runtime.onInstalled.addListener(() => {
 
 // listen to requests (from popup)
 chrome.runtime.onMessage.addListener(async (message, sender, sendResponse) => {
-    if (message.type === "summarization_request"){
+    if (message.type === "summarization_request") {
         let tabId = message.tabId
         let url = message.url
         let text = await getSelectedText(tabId)
@@ -18,7 +18,7 @@ chrome.runtime.onMessage.addListener(async (message, sender, sendResponse) => {
     }
 })
 
-async function saveSummaryToSummaryHistory(url, text, summary){
+async function saveSummaryToSummaryHistory(url, text, summary) {
     let history = await getSummaryHistory()
     history.push({
         url: url,
@@ -29,36 +29,36 @@ async function saveSummaryToSummaryHistory(url, text, summary){
 
 }
 
-async function overwriteSummaryHistory(newHistory){
+async function overwriteSummaryHistory(newHistory) {
     await chrome.storage.local.set({
         history: newHistory
     })
 }
 
-async function getSummaryHistory(){
-    let result  = await chrome.storage.local.get("history")
+async function getSummaryHistory() {
+    let result = await chrome.storage.local.get("history")
     return result.history;
 }
 
-async function getSelectedText(tabId){
+async function getSelectedText(tabId) {
     let injectionResults = await chrome.scripting.executeScript({
-        target: {tabId: tabId},
+        target: { tabId: tabId },
         func: () => window.getSelection().toString(),
     })
-    if(injectionResults && injectionResults.length >= 1){
+    if (injectionResults && injectionResults.length >= 1) {
         let selection = injectionResults[0].result
         return selection
     }
     else return ""
 }
 
-async function summarizeText(text){
+async function summarizeText(text) {
     return await summarizeTextWithHuggingFace(text)
 }
 
-async function summarizeTextWithHuggingFace(text){
-    let result = await queryHuggingFace({"inputs": text})
-    if(result && result.length >= 1){
+async function summarizeTextWithHuggingFace(text) {
+    let result = await queryHuggingFace({ "inputs": text })
+    if (result && result.length >= 1) {
         let summary = result[0]["summary_text"]
         return JSON.stringify(summary);
     }
@@ -68,19 +68,19 @@ async function summarizeTextWithHuggingFace(text){
 async function queryHuggingFace(data) {
     let apiKey = await getApiKey()
     console.log("query hugging face with api key", apiKey)
-	const response = await fetch(
-		"https://api-inference.huggingface.co/models/facebook/bart-large-cnn",
-		{
-			headers: { Authorization: apiKey},
-			method: "POST",
-			body: JSON.stringify(data),
-		}
-	);
-	const result = await response.json();
-	return result;
+    const response = await fetch(
+        "https://api-inference.huggingface.co/models/facebook/bart-large-cnn",
+        {
+            headers: { Authorization: apiKey },
+            method: "POST",
+            body: JSON.stringify(data),
+        }
+    );
+    const result = await response.json();
+    return result;
 }
 
-async function getApiKey(){
+async function getApiKey() {
     let result = await chrome.storage.local.get("apiKey")
     return result.apiKey
-  }
+}
