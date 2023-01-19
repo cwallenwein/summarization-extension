1e1b9f82ef5b7ded5ac2a4b141f8162967833045
cwallenwein <chris.wallenwein@gmail.com>
2023-01-19 15:24:32 +0100

    summarize text using user-specified API key

--- worker.js
+++ worker.js
@@ -2,14 +2,17 @@
 chrome.runtime.onInstalled.addListener(() => {
     chrome.storage.local.set({
         history: [
-            {
+            /*{
                 url: "https://google.com",
+                text: "This is a long text",
                 summary: "This is a summary"
             },{
                 url: "https://youtube.com",
+                text: "This is another long text",
                 summary: "This is another summary"
-            }
-        ]
+            }*/
+        ],
+        apiKey: undefined
     })
 })
 
@@ -17,11 +20,35 @@ chrome.runtime.onInstalled.addListener(() => {
 chrome.runtime.onMessage.addListener(async (message, sender, sendResponse) => {
     if (message.type === "summarization_request"){
         let tabId = message.tabId
-        console.log("Summarization request from tab", tabId)
+        let url = message.url
         let text = await getSelectedText(tabId)
-        console.log(text)
+        // TODO: check if no text is selected
+        let summary = await summarizeText(text)
+        await saveSummaryToSummaryHistory(url, text, summary)
     }
 })
+
+async function saveSummaryToSummaryHistory(url, text, summary){
+    let history = await getSummaryHistory()
+    history.push({
+        url: url,
+        text: text,
+        summary: summary
+    })
+    await overwriteSummaryHistory(history)
+
+}
+
+async function overwriteSummaryHistory(newHistory){
+    await chrome.storage.local.set({
+        history: newHistory
+    })
+}
+
+async function getSummaryHistory(){
+    let result  = await chrome.storage.local.get("history")
+    return result.history;
+}
 
 async function getSelectedText(tabId){
     let injectionResults = await chrome.scripting.executeScript({
@@ -34,3 +61,36 @@ async function getSelectedText(tabId){
     }
     else return ""
 }
+
+async function summarizeText(text){
+    return await summarizeTextWithHuggingFace(text)
+}
+
+async function summarizeTextWithHuggingFace(text){
+    let result = await queryHuggingFace({"inputs": text})
+    if(result && result.length >= 1){
+        let summary = result[0]["summary_text"]
+        return JSON.stringify(summary);
+    }
+    else return "Error"
+}
+
+async function queryHuggingFace(data) {
+    let apiKey = await getApiKey()
+    console.log("query hugging face with api key", apiKey)
+	const response = await fetch(
+		"https://api-inference.huggingface.co/models/facebook/bart-large-cnn",
+		{
+			headers: { Authorization: apiKey},
+			method: "POST",
+			body: JSON.stringify(data),
+		}
+	);
+	const result = await response.json();
+	return result;
+}
+
+async function getApiKey(){
+    let result = await chrome.storage.local.get("apiKey")
+    return result.apiKey
+  }
