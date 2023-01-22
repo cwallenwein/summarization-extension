e63f0fac93c761d46d34740ba9327942fe0492c0
cwallenwein <chris.wallenwein@gmail.com>
2023-01-22 13:06:34 +0100

    switch to React.js and ant.design

---
+++ src/App.tsx
@@ -0,0 +1,163 @@
+import React, { useEffect, useState } from 'react';
+import type { FC } from 'react';
+import { CopyOutlined, HighlightOutlined, LeftOutlined, FileTextOutlined, DeleteOutlined, EditOutlined, EllipsisOutlined, LinkOutlined, SettingOutlined, QuestionCircleOutlined, KeyOutlined, ApiOutlined } from '@ant-design/icons';
+import { Avatar, Button, Card, Col, Row, Skeleton, Typography, Tooltip } from 'antd';
+import 'antd/dist/reset.css';
+import './App.css';
+
+const { Title, Paragraph, Link, Text } = Typography;
+
+// TODO: Test if API key is valid by sending an example request
+
+const App: React.FC = () => {
+  const [activeTab, setActiveTab] = useState<string>("main")
+
+  const tabs: { [key: string]: any } = {
+    main: <SummaryHistory></SummaryHistory>,
+    settings: <Settings></Settings>,
+    howToUse: <HowToUse></HowToUse>
+  }
+
+  const [loading, setLoading] = useState<boolean>(false);
+
+  // <Button icon={<ApiOutlined />} onClick={() => setActiveTab("settings")}>Settings</Button>
+  // <Button icon={<QuestionCircleOutlined />} onClick={() => setActiveTab("howToUse")}>How to Use</Button>
+
+  return (
+    <div className="App">
+      <Card
+        id="main"
+        title={<ExtensionTitle activeTab={activeTab} setActiveTab={setActiveTab} />}
+        actions={[
+          <Tooltip title="Set HuggingFace API Key"><ApiOutlined onClick={() => setActiveTab("settings")} /></Tooltip>,
+          <Tooltip title="How to use this extension"><QuestionCircleOutlined onClick={() => setActiveTab("howToUse")} /></Tooltip>]
+        }
+        extra={< Tooltip title="Summarize Highlighted Text" > <Button type="primary" loading={loading} onClick={() => setLoading(true)}>Summarize Text</Button></Tooltip >}
+        style={{ textAlign: "left" }}>
+        <div style={{ overflowY: "auto", height: 400, width: 500 }}> {tabs[activeTab]} </div>
+      </Card >
+    </div >
+  )
+};
+
+const ExtensionTitle: any = (props: { activeTab: string, setActiveTab: any }) => {
+  const [hidden, setHidden] = useState<boolean>(false)
+  const icon = <LeftOutlined />
+  const style = { marginRight: 10 }
+  const onClick = () => props.setActiveTab("main")
+
+  useEffect(() => { if (props.activeTab === "main") { setHidden(true) } else { setHidden(false) } }, [props.activeTab])
+
+  return (
+    <>
+      <Button icon={icon} hidden={hidden} style={style} onClick={onClick} />
+      Highlighter
+    </>
+  )
+}
+
+// TODO Add Button to go back to summaries
+const HowToUse: React.FC = () => {
+  return (
+    <div>
+      <Paragraph>
+        Explanation of how to use this extension
+      </Paragraph>
+    </div>
+  )
+}
+
+// TODO Add Button to go back to summaries
+const Settings: React.FC = () => {
+  const [apiKey, setApiKey] = useState<string>("")
+
+  // useEffect(() => {
+  // getApiKey().then((result) => {
+  // setApiKey(result)
+  // })
+  // })
+
+  // <Input placeholder="API Key" value={apiKey} onChange={(e) => setApiKey(e.target.value)} />
+
+  return (
+    <div>
+      <Paragraph>
+        Save and change your API Key here
+      </Paragraph>
+    </div>
+  )
+}
+
+
+function SummaryHistory() {
+  const [history, setHistory] = useState<string[]>([])
+
+  useEffect(() => {
+    getHistory().then((result) => {
+      setHistory(result)
+    })
+  })
+
+  return (
+    <>
+      {
+        history.map((item: any, index) => {
+          return <SummaryCard content={item.summary} url={item.url}>
+          </SummaryCard>
+        })
+      }
+    </>
+
+  )
+}
+
+// TODO remove HTTPs from url
+// TODO add loading state
+function SummaryCard(props: any) {
+  const [loading, setLoading] = useState(false);
+
+  return (
+    <Card
+      style={{
+        width: "calc(100% - 32px)",
+        textAlign: "left",
+        margin: "16px"
+      }}
+
+      actions={[
+        <Tooltip title="Copy Summary"><CopyOutlined /></Tooltip>,
+        //<Tooltip title="Link to Original Text"><FileTextOutlined /></Tooltip>,
+        <Tooltip title="Delete Summary"><DeleteOutlined /></Tooltip>
+      ]}
+    >
+      <Skeleton loading={loading} active>
+        <Typography>
+          <Tooltip title={props.url}>
+            <Text>
+              <Link href={props.url} target="_blank" style={{ fontSize: "12pt" }}>
+                {props.url}
+                <LinkOutlined style={{ marginLeft: "5px" }} />
+              </Link>
+            </Text>
+          </Tooltip>
+          <Paragraph>
+            {props.content}
+          </Paragraph>
+        </Typography>
+      </Skeleton>
+
+    </Card>
+  )
+
+}
+
+async function getHistory() {
+  try {
+    let result = await chrome.storage.local.get("history")
+    return result.history
+  } catch (error) {
+    console.error(error)
+  }
+}
+
+export default App;
--- worker.js
+++ worker.js
@@ -5,7 +5,28 @@ chrome.runtime.onMessage.addListener(handleMessage)
 async function initializeExtension() {
     try {
         await chrome.storage.local.set({
-            history: [],
+            history: [
+                {
+                    url: "https://us.cnn.com/2023/01/19/tech/chatgpt-future-davos",
+                    text: "This is the first test text",
+                    summary: "Jeff Maggioncalda, the CEO of online learning provider Coursera, said that when he first tried ChatGPT, he was “dumbstruck.” Now, it’s part of his daily routine.He uses the powerful new AI chatbot tool to bang out emails. He uses it to craft speeches “in a friendly, upbeat, authoritative tone with mixed cadence.” He even uses it to help break down big strategic questions — such as how Coursera should approach incorporating artificial intelligence tools like ChatGPT into its platform.“I use it as a writing assistant and as a thought partner,” Maggioncalda told CNN."
+                },
+                {
+                    url: "google.com",
+                    text: "This is the second test text",
+                    summary: "Aunt Nicki is hard of hearing. Although there are many enhanced listening devices available to help her, such as an Assistive Living amplifier or a closed captioning screen that sits in a cup holder, she tells me they don’t work well enough. When I recently tried on the new “smart caption glasses” at the Royal National Theatre, I had her on my mind. The theater is testing a pilot program for the technology for all performances of Hadestown and War Horse through October. It plans to make the glasses available for all of its performances during the 2019 season."
+                },
+                {
+                    url: "google.com",
+                    text: "This the third test text",
+                    summary: "Chinese tech giant Baidu revealed its plan to roll out Apollo RT6, an autonomous self-driving car with a detachable steering wheel, even though Chinese authorities haven't approved the concept yet. In August, Baidu obtained the first permits to operate fully driverless robotaxis in two cities in China."
+                },
+                {
+                    url: "google.com",
+                    text: "This is the fourth test text",
+                    summary: "Smith, who was managing 40 assistants to financial advisers at a bank in Atlanta, said most of her team performed well during the past year and were great about staying in touch with her. But the few who were not exhausted her."
+                }
+            ],
             apiKey: undefined
         })
     } catch (error) {
