21f9a5328be1fc9a7d1189d0c97fb4ee719766cf
cwallenwein <chris.wallenwein@gmail.com>
2023-01-26 12:28:32 +0100

    make title a component

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import type { FC } from 'react';
-import { CopyOutlined, ArrowLeftOutlined, DeleteOutlined, LinkOutlined, QuestionOutlined, SettingOutlined, ApiOutlined } from '@ant-design/icons';
-import { Button, Card, Form, Input, Skeleton, Typography, Tooltip, message } from 'antd';
+import { Card } from 'antd';
 import Storage, { ISummary } from './services/Storage';
 import { Help } from "./components/Help"
 import { Settings } from "./components/Settings"
@@ -11,11 +10,17 @@ import { RequestSummaryButton } from "./components/RequestSummaryButton"
 import 'antd/dist/reset.css';
 import './App.css';
 
-// TODO: Test if API key is valid by sending an example request
-// TODO: remove " " from summary text
 // TODO Propper url shortening in header of summary
+// TODO: remove " " from summary text
+// TODO: Test if API key is valid by sending an example request
 // TODO: message when no text is selected
-// TODO move components to different files
+// TODO add types
+// TODO add documentation
+// TODO when summarize is clicked, immediately add loading summary to history. When response is received, update the summary in history and remove loading state from button
+// TODO don't add summary to history if summarization failed
+// TODO move header to card header
+// TODO warning when no text is selected
+// TODO warning if text could not be selected
 
 const App: React.FC = () => {
   const [activeTab, setActiveTab] = useState<string>("main")
@@ -30,7 +35,7 @@ const App: React.FC = () => {
     <div className="App">
       <Card
         id="main"
-        title={activeTab !== "main" ? <><BackButton setActiveTab={setActiveTab} /> Highlighter</> : <>Highlighter</>}
+        title={<Title activeTab={activeTab} />}
         extra={[
           <GoToSettingsButton setActiveTab={setActiveTab} />,
           <GoToHelpButton setActiveTab={setActiveTab} />
@@ -42,6 +47,16 @@ const App: React.FC = () => {
     </div >
   )
 };
+
+const Title = (props: any) => {
+  // If the current tab is not "main", show back button otherwise only show title
+  if (props.activeTab !== "main") {
+    return (<><BackButton setActiveTab={props.setActiveTab} /> Highlighter</>)
+  } else {
+    return (<>Highlighter</>)
+  }
+
+}
 
 function Summaries() {
   const [allSummaries, setAllSummaries] = useState<ISummary[]>([])
